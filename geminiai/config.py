ea38dfdab228e0942665556b6f22a6acbfcfacc4
Ryan3954 <ryan.3954.ryan@gmai.com>
2024-12-18 23:31:07 +0700

    remove api key

--- geminiai/config.py
+++ geminiai/config.py
@@ -1,13 +1,13 @@
-pinecone_api = ""
-pinecone_region = ""
-pinecone_db = ""
-pinecone_dimenstion = 0
-pinecone_index_name = ""
-pinecone_index_host = ""
+pinecone_api = "pcsk_6UrqME_77Zb6THd4igPEaH92Yn7LRcSf65DoTtZz8gm9MSaWGwq6J4gRyvg6rjn4dSL735"
+pinecone_region = "us-west1-gcp"
+pinecone_db = "angelai"
+pinecone_dimenstion = 384
+pinecone_index_name = "angelai"
+pinecone_index_host = "https://angelai-bbnnx1q.svc.aped-4627-b74a.pinecone.io"
 
-geminiai_api = ""
+geminiai_api = "AIzaSyAw7PcCbdWQ61j8GsOfI0wq-bQD-irK0cQ"
 
-db_user = ""
-mysql_db = ""
+db_user = "root"
+mysql_db = "angelai"
 
-query = ""
+query = "SELECT InjectionId, InjectionName, InjectionDescription FROM tablepromptinjection"
