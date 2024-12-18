a35e98e6db4940075f88dcb0107ff69dd811d780
Ryan3954 <ryan.3954.ryan@gmai.com>
2024-12-18 23:32:13 +0700

    hapus api

--- geminiai/config.py
+++ geminiai/config.py
@@ -1,13 +1,13 @@
-pinecone_api = "pcsk_6UrqME_77Zb6THd4igPEaH92Yn7LRcSf65DoTtZz8gm9MSaWGwq6J4gRyvg6rjn4dSL735"
-pinecone_region = "us-west1-gcp"
-pinecone_db = "angelai"
-pinecone_dimenstion = 384
-pinecone_index_name = "angelai"
-pinecone_index_host = "https://angelai-bbnnx1q.svc.aped-4627-b74a.pinecone.io"
+pinecone_api = ""
+pinecone_region = ""
+pinecone_db = ""
+pinecone_dimenstion = 0
+pinecone_index_name = ""
+pinecone_index_host = ""
 
-geminiai_api = "AIzaSyAw7PcCbdWQ61j8GsOfI0wq-bQD-irK0cQ"
+geminiai_api = ""
 
-db_user = "root"
-mysql_db = "angelai"
+db_user = ""
+mysql_db = ""
 
-query = "SELECT InjectionId, InjectionName, InjectionDescription FROM tablepromptinjection"
+query = ""
