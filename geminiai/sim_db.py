af46047442eb5eb4391c52c0f0ca42b2f780ea39
Ryan3954 <ryan.3954.ryan@gmai.com>
2024-12-22 22:37:28 +0700

    optimize speed

--- geminiai/sim_db.py
+++ geminiai/sim_db.py
@@ -4,12 +4,13 @@ from sentence_transformer import generate_embeddings, generate_embedding
 import config
 
 def sim_check(user_query):
-    data1, _ = fetch_data()
-    embeddings = generate_embeddings(data1)
+    # data1, _ = fetch_data() (uncomment if pinecone does not have vector data, uncomment if pinecone index is new)
+
+    # embeddings = generate_embeddings(data1) (uncomment if pinecone does not have vector data, uncomment if pinecone index is new)
 
     index = initialize_pinecone(config.pinecone_api, config.pinecone_region, config.pinecone_db, dimension=config.pinecone_dimenstion)
 
-    upsert_to_pinecone(index, embeddings)
+    # upsert_to_pinecone(index, embeddings) (uncomment if pinecone does not have vector data, uncomment if pinecone index is new)
 
     query_embedding = generate_embedding(user_query).tolist()
 
